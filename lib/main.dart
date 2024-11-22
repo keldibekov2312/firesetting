d098ceef46d10deeb61fdddc688e4df5c424a2e5
user <keldibekovyahyobek7@gmail.com>
2024-11-22 18:38:15 +0500

    firesettings

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'pages/home_page.dart';
+
+
+void main() async{
+
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+      ),
+      home: HomePage()
+    );
+  }
+}
+
